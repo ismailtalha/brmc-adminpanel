4fd67d700683f5770bd17d273ce80663ba3d55ef
HP_LT1042\admin <ismail.talha@qavitechnologies.com>
2021-08-10 01:09:52 +0500

    enviroment change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://backend.smokesinc.com.192-185-10-74.hgws14.hgwin.temp.domains/brmsapi/'
+  url: 'https://backend.smokesinc.com.192-185-10-74.hgws14.hgwin.temp.domains/brmsapi/'
 };
